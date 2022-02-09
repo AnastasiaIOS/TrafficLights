ae71ebc6b01548d5c0f268ee49dd6b789bf9c403
Anastasia <pavlova.a.s@inbox.ru>
2022-02-10 01:28:44 +0300

    create project

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redCircleView: UIView!
+    @IBOutlet var yellowCircleView: UIView!
+    @IBOutlet var greenCircleView: UIView!
+    
+    @IBOutlet var clickButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redCircleView.alpha = 0.3
+        yellowCircleView.alpha = 0.3
+        greenCircleView.alpha = 0.3
+        
+        redCircleView.layer.cornerRadius = 45
+        yellowCircleView.layer.cornerRadius = 45
+        greenCircleView.layer.cornerRadius = 45
+        
+        clickButton.layer.cornerRadius = 20
     }
 
-
+    
+    @IBAction func showTextButtonPressed() {
+        
+        
+    }
+    
 }
 
