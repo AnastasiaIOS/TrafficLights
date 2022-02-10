3ae03c03a7e30d9e879cd977164c6b49de8fc720
Anastasia <pavlova.a.s@inbox.ru>
2022-02-10 23:50:23 +0300

    add button and code

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -13,22 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet var redCircleView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenCircleView: UIView!
+    @IBOutlet var textOnTheButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         redCircleView.alpha = 0.3
         yellowView.alpha = 0.3
+        greenCircleView.alpha = 0.3
         
         
-        redCircleView.layer.cornerRadius = 45
-        yellowView.layer.cornerRadius = 45
-        greenCircleView.layer.cornerRadius = 45
+        redCircleView.layer.cornerRadius = 90
+        yellowView.layer.cornerRadius = 90
+        greenCircleView.layer.cornerRadius = 90
         
+        textOnTheButton.layer.cornerRadius = 12
+        textOnTheButton.setTitle("START", for: .normal)
         
     }
 
     
+    @IBAction func pushAndChangeTextButton() {
+        
+        redCircleView.alpha = 1
+        textOnTheButton.setTitle("NEXT", for: .normal)
+        
+        
+        
+    }
     
     
 }
