b35610df9ad37e2f0d7f219699d86680f5c3282b
Anastasia <pavlova.a.s@inbox.ru>
2022-02-13 20:38:30 +0300

    update from teacher's comments

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet var redCircleView: UIView!
     @IBOutlet var yellowView: UIView!
@@ -23,42 +23,52 @@ class ViewController: UIViewController {
         greenCircleView.alpha = 0.3
         
         
-        redCircleView.layer.cornerRadius = 90
-        yellowView.layer.cornerRadius = 90
-        greenCircleView.layer.cornerRadius = 90
+        redCircleView.layer.cornerRadius = redCircleView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenCircleView.layer.cornerRadius = greenCircleView.frame.width / 2
         
         textOnTheButton.layer.cornerRadius = 12
         textOnTheButton.setTitle("START", for: .normal)
         
     }
-
-   
-        
-    //высылаю ДЗ "как есть", т.к. уже всю голову сломала над задачей. помогите пожалуйста ее решить в текущей реализации, если конечно можно ее так сделать (подсмотрела в разбор дз, там сделано иначе)
-        
+    
     
     
     @IBAction func pushAndChangeTextButton() {
         
-        textOnTheButton.setTitle("NEXT", for: .normal)
-        //redCircleView.alpha = 1
+        if textOnTheButton.currentTitle == "START" {
+            textOnTheButton.setTitle("NEXT", for: .normal)
+            redCircleView.alpha = 1
+            return
+        }
         
         changeColorView()
+    }
+    
+    private func changeColorView () {
         
-        func changeColorView () {
-            
-            if yellowView.alpha == 0.3 && greenCircleView.alpha == 0.3 {
-                redCircleView.alpha = 1 }
-                
-                if redCircleView.alpha == 1 {
-                    yellowView.alpha = 1
-                    redCircleView.alpha = 0.3}
-            
-                    if yellowView.alpha == 1 {
-                        greenCircleView.alpha = 1
-                        yellowView.alpha = 0.3 }
-                 }
+        if yellowView.alpha == 0.3 && greenCircleView.alpha == 0.3 {
+            redCircleView.alpha = 1
+            return
+        }
+        
+        if redCircleView.alpha == 1 {
+            yellowView.alpha = 1
+            redCircleView.alpha = 0.3
+            return
+        }
+        
+        if yellowView.alpha == 1 {
+            greenCircleView.alpha = 1
+            yellowView.alpha = 0.3
+            return
+        }
+        
+        if greenCircleView.alpha == 1 {
+            greenCircleView.alpha = 0.3
+            redCircleView.alpha = 1
+            return
         }
         
     }
-
+}
